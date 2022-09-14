e277ad22a00fd29f1def721b0e06d62c4b28fb52
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-09-14 22:35:41 +0530

    Add files via upload

---
+++ Type_casting_conversion.py
@@ -0,0 +1,59 @@
+# a=5
+# print(type(a))
+# print(float(a))
+# print(str(a))
+
+# b=7.8
+# print(type(b))
+# print(int(b))
+# print(str(b))
+
+# c='hi'
+# print(int(c))
+# print(float(c))
+
+# d='5'
+# print(type(d))
+# print(int(d))
+# print(float(d))
+
+# e='7.8'
+# print(type(e))
+# print(int(e))
+# print(float(e))
+# f=float(e)
+# print(int(f))
+
+
+# a=1 ; b=5
+# print(a+b)
+
+# c=7 ; d=3.4
+# print(c+d)
+
+# e='hi' ; f=5 ; g=6.7
+# print(type(e))
+# print(type(f))
+# print(e+str(f))
+# print(e+str(g))
+# print(e+'f')
+# print(e+'5')
+# print(f+g)
+# print(int(f+g))
+
+# print(1,2,3,4,5)
+# print(1,2,3,4,5,sep='')
+# print(1,2,3,4,5,sep=' ')
+# print(1,2,3,4,5,sep='  ')
+# print(1,2,3,4,5,sep=' -> ')
+
+# print('hi','world')
+# print('hi','world',sep='  ')
+
+# print('hi',6)
+# print('hi'+''+str(6))
+# print('hi'+' '+str(6))
+
+# print(1,2,3)
+# print('1'+'  '+'2'+'  '+'3')
+# print(1,2,3,4,5,6,7,8,9,10,sep=' ')
